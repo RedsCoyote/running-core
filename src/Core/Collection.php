f75132186b5f99b81c35d8bdb39129fa664d8acd
Степанцев Альберт <126@gosbroker.ru>
2016-11-07 16:50:29 +0300

    + Collection class

---
+++ src/Core/Collection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Running\Core;
+
+/**
+ * Collection class
+ *
+ * Class Collection
+ * @package Running\Core
+ */
+class Collection
+    implements ICollection
+{
+    use TCollection;
+}
---
+++ tests/Core/CollectionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Running\tests\Core\Collection;
+
+use Running\Core\Collection;
+use Running\Core\ICollection;
+
+class CollectionTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testConstruct()
+    {
+        $collection = new Collection([100, 200, 300, [400, 500]]);
+
+        $this->assertInstanceOf(Collection::class, $collection);
+        $this->assertInstanceOf(ICollection::class, $collection);
+        $this->assertCount(4, $collection);
+        $this->assertEquals(
+            [100, 200, 300, new Collection([400, 500])],
+            $collection->getData()
+        );
+        $this->assertEquals(
+            [100, 200, 300, [400, 500]],
+            $collection->toArray()
+        );
+        $this->assertEquals(100, $collection[0]);
+        $this->assertEquals(200, $collection[1]);
+        $this->assertEquals(300, $collection[2]);
+        $this->assertEquals(new Collection([400, 500]), $collection[3]);
+    }
+
+}
