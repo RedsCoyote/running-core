853382f7a3e6f0cb1dcfb6a6222b7911cffc9a10
RedsCoyote <mail@mail.com>
2017-03-02 12:11:37 +0300

    refactor for scalar-typed collection

--- tests/Core/TypedCollectionTraitTest.php
+++ tests/Core/TypedCollectionTraitTest.php
@@ -2,7 +2,6 @@
 
 namespace Running\tests\Core\TypedCollectionTrait;
 
-use Running\Core\TypedCollection;
 use Running\Core\TypedCollectionInterface;
 use Running\Core\TypedCollectionTrait;
 
@@ -13,6 +12,36 @@ class testClass
     public static function getType()
     {
         return testValueClass::class;
+    }
+}
+
+class testFloatClass
+    implements TypedCollectionInterface
+{
+    use TypedCollectionTrait;
+    public static function getType()
+    {
+        return 'float';
+    }
+}
+
+class testIntClass
+    implements TypedCollectionInterface
+{
+    use TypedCollectionTrait;
+    public static function getType()
+    {
+        return 'int';
+    }
+}
+
+class testBoolClass
+    implements TypedCollectionInterface
+{
+    use TypedCollectionTrait;
+    public static function getType()
+    {
+        return 'bool';
     }
 }
 
@@ -39,7 +68,7 @@ class TypedCollectionTraitTest extends \PHPUnit_Framework_TestCase
      */
     public function testInvalidClassConstruct()
     {
-        $collection = new testClass([1, 2, 3]);
+        new testClass([1, 2, 3]);
     }
 
     /**
@@ -89,4 +118,73 @@ class TypedCollectionTraitTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(5, $collection);
     }
 
+
+    /**
+     * @expectedException \Running\Core\Exception
+     * @expectedExceptionMessage Typed collection class mismatch
+     */
+    public function testInvalidIntegerClass()
+    {
+        $collection = new testClass();
+        $collection->append('42');
+    }
+
+    public function testValidIntegerClass()
+    {
+        $collection = new testIntClass([1, 2]);
+        $this->assertInstanceOf(TypedCollectionInterface::class, $collection);
+        $this->assertCount(2, $collection);
+        $collection->append(3);
+        $this->assertCount(3, $collection);
+        $collection->prepend(4);
+        $this->assertCount(4, $collection);
+        $collection[] = 5;
+        $this->assertCount(5, $collection);
+    }
+
+    /**
+     * @expectedException \Running\Core\Exception
+     * @expectedExceptionMessage Typed collection class mismatch
+     */
+    public function testInvalidFloatClass()
+    {
+        $collection = new testClass();
+        $collection->append(true);
+    }
+
+    public function testValidFloatClass()
+    {
+        $collection = new testFloatClass([1.0, 2.1]);
+        $this->assertInstanceOf(TypedCollectionInterface::class, $collection);
+        $this->assertCount(2, $collection);
+        $collection->append(3.2);
+        $this->assertCount(3, $collection);
+        $collection->prepend(4.3);
+        $this->assertCount(4, $collection);
+        $collection[] = 5.4;
+        $this->assertCount(5, $collection);
+    }
+
+    /**
+     * @expectedException \Running\Core\Exception
+     * @expectedExceptionMessage Typed collection class mismatch
+     */
+    public function testInvalidBooleanClass()
+    {
+        $collection = new testClass();
+        $collection->append('42');
+    }
+
+    public function testValidBooleanClass()
+    {
+        $collection = new testBoolClass([true, true]);
+        $this->assertInstanceOf(TypedCollectionInterface::class, $collection);
+        $this->assertCount(2, $collection);
+        $collection->append(false);
+        $this->assertCount(3, $collection);
+        $collection->prepend(true);
+        $this->assertCount(4, $collection);
+        $collection[] = false;
+        $this->assertCount(5, $collection);
+    }
 }
